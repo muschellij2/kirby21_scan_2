c5187aa5fa1e1a34e3338efe656c5f69633052bd
muschellij2 <muschellij2@gmail.com>
2015-04-08 01:54:07 -0400

    Added scans

---
+++ R/Kirby21_data.R
@@ -0,0 +1,8 @@
+#' @name Kirby 21 Data
+#' @title Kirby 21 Visit 1 data
+#'
+#' @description A dataset containing 3 images (FLAIR, MPRAGE, T2w) for 21 subjects
+#' from Kennedy Krieger
+#'
+#' @source \url{https://www.nitrc.org/frs/?group_id=313}
+NULL
